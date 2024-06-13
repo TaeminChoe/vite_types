c6cb3bd6aa9dc1a721d1af8b83a0274ebb01467e
[파인원]최태민 <xoals628@pineone.com>
2024-06-13 15:57:32 +0900

    install basic lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; //추가
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    /**
+     * 컴포넌트를 참조할 때, 절대경로를 참조하지 않아도 되는 편의성을 제공
+     */
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
